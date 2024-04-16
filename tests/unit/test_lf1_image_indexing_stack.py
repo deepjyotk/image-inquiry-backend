9112ddfa6b2f19d9fba0e4fddd151998f4aab9f5
Deepjyot Singh Kapoor <kapoordeepjyotsingh29@gmail.com>
2024-04-15 21:20:34 -0400

    CDK template python

---
+++ tests/unit/test_lf1_image_indexing_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from lf1_image_indexing.lf1_image_indexing_stack import Lf1ImageIndexingStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in lf1_image_indexing/lf1_image_indexing_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = Lf1ImageIndexingStack(app, "lf1-image-indexing")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
